fcce7236bfd344fffc2742d36e7d47d5a6135344
Jan Ručil <rucil.jan@gmail.com>
2020-06-20 16:08:58 +0200

    .

--- js/Index.html.js
+++ js/Index.html.js
@@ -2,9 +2,6 @@
 canvas.style.opacity = 1;
 canvas.style.width = '100%'; 
 
-setTimeout(
-    function(){    
-        document.getElementById('almost').style.fontSize = document.getElementById('sd').style.fontSize;
-        },4500);
+
 
 
