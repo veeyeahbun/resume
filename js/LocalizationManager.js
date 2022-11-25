a65e542d4230c28abb76078e9c175abb73d55477
Jan Ručil <rucil.jan@gmail.com>
2022-11-25 13:12:07 +0100

    .

--- js/LocalizationManager.js
+++ js/LocalizationManager.js
@@ -41,7 +41,7 @@ document.getElementById('my_services').innerHTML = "Über mich";
 document.getElementById('my_contact').innerHTML = "Kontakt";
 document.getElementById('contact_head').innerHTML = "Kontakt";
 
-document.getElementById('caption').innerHTML = "Software und 3D Grafik";
+document.getElementById('caption').innerHTML = "Programmierer und 3D-Grafiker";
 
 document.getElementById('my_policy').innerHTML = "<a href=\"datenschutz.html\" target=\"_blank\"  class=\"smoothScroll\">Datenschutz</a>";
 
@@ -68,19 +68,13 @@ document.getElementById('my_policy').innerHTML = "<a href=\"datenschutz.html\" t
 
     document.getElementById('games_header').innerHTML = "Was ich mache";
     document.getElementById('gamedevInfo').innerHTML = "0 published titles, work in progress";
-    document.getElementById('3dvis').innerHTML = "3D visualisation";
-    document.getElementById('3dvisInfo').innerHTML = "Custom 3D assets, renders, simulations and VR";
+    document.getElementById('3dvisInfo').innerHTML = "3D-Asset-Erstellung, Rendering, interaktive Simulationen und VR";
 
-    document.getElementById('cnetInfo').innerHTML = "Non-gaming server - client applications & utilities";
+    document.getElementById('cnetInfo').innerHTML = "Server - client applications & utilities";
 
-    document.getElementById('specialist').innerHTML = "Erfahrener Programmierer";
+    document.getElementById('specialist').innerHTML = "Über mich";
    
   
-    document.getElementById('graphic').innerHTML = "3D Grafik";
-    document.getElementById('graphic_p').innerHTML = "Ich beschäftige mich auch mit 3D Grafik & Spielentwicklung";
-
-
-
     
     document.getElementById('inboxme').innerHTML = "Schreiben Sie mir:";
     document.getElementById('wien').innerHTML = "Wien, Österreich";
@@ -101,7 +95,7 @@ document.getElementById('my_references').innerHTML = "Specializace";
 document.getElementById('my_services').innerHTML = "O mě";
 document.getElementById('my_contact').innerHTML = "Kontakt";
 document.getElementById('contact_head').innerHTML = "Kontakt";
-document.getElementById('caption').innerHTML = "Software a 3D grafika";
+document.getElementById('caption').innerHTML = "Programátor a 3D grafik";
 document.getElementById('my_policy').innerHTML = "<a href=\"ochranadat.html\" target=\"_blank\"  class=\"smoothScroll\">Ochrana osobních údajů</a>";
     document.getElementById('2real').innerHTML = "Freelancer";
 
@@ -116,20 +110,16 @@ document.getElementById('my_policy').innerHTML = "<a href=\"ochranadat.html\" ta
    
     document.getElementById('kontakt').innerHTML = "Pro více informací mě kontaktujte přes <a style='text-decoration: underline;' href='mailto:office@janrucil.dev'>email</a>.";
     document.getElementById('games_header').innerHTML = "Co dělám";
-
-    document.getElementById('gamedev').innerHTML = "Hry";
     document.getElementById('gamedevInfo').innerHTML = "0 vydaných titulů, pracuji na tom";
-    document.getElementById('3dvis').innerHTML = "3D vizualizace";
-    document.getElementById('3dvisInfo').innerHTML = "3D assety, rendery, simulace a VR na míru.";
+    document.getElementById('3dvisInfo').innerHTML = "Tvorba 3D assetů, rendering, interaktivní simulace a VR.";
 
-    document.getElementById('cnetInfo').innerHTML = "Neherní server - client aplikace & nástroje";
+    document.getElementById('cnetInfo').innerHTML = "Server - client aplikace & nástroje";
 
 
-    document.getElementById('specialist').innerHTML = "Zkušený programátor";
+    document.getElementById('specialist').innerHTML = "O mě";
     document.getElementById('specialist_p').innerHTML = "Mám několikaleté zkušenosti s vývojem obchodního software";
 
-    document.getElementById('graphic').innerHTML = "3D grafik";
-    document.getElementById('graphic_p').innerHTML = "Zabývám se také 3D grafice & vývoji videoher.";
+
 
     document.getElementById('inboxme').innerHTML = "Napište mi:";
     document.getElementById('wien').innerHTML = "Vídeň, Rakousko";
@@ -152,7 +142,7 @@ document.getElementById('my_services').innerHTML = "About me";
 document.getElementById('my_contact').innerHTML = "Contact";
 document.getElementById('contact_head').innerHTML = "Contact";
 document.getElementById('my_policy').innerHTML = "<a href=\"privacy.html\" target=\"_blank\"  class=\"smoothScroll\">Privacy policy</a>";
-document.getElementById('caption').innerHTML = "Software and 3D graphics";
+document.getElementById('caption').innerHTML = "Programmer and 3D artist";
     document.getElementById('2real').innerHTML = "Freelancer";
     document.getElementById('zabranskyTimeline').innerHTML = "01/2019 - present";
 
@@ -169,20 +159,13 @@ document.getElementById('caption').innerHTML = "Software and 3D graphics";
     document.getElementById('kontakt').innerHTML = "For more info contact me per <a style='text-decoration: underline;' href='mailto:office@janrucil.dev'>email</a>.";  
 
     document.getElementById('games_header').innerHTML = "What I do";
-
-    document.getElementById('gamedev').innerHTML = "Games";
     document.getElementById('gamedevInfo').innerHTML = "0 published titles, work in progress";
-    document.getElementById('3dvis').innerHTML = "3D visualisation";
-    document.getElementById('3dvisInfo').innerHTML = "Custom 3D assets, renders, simulations and VR";
+    document.getElementById('3dvisInfo').innerHTML =  "Creation of 3D assets, rendering, interactive simulations and VR";
 
-    document.getElementById('cnetInfo').innerHTML = "Non-gaming server - client applications & utilities";
+    document.getElementById('cnetInfo').innerHTML = "Server - client applications & utilities";
 
-    document.getElementById('specialist').innerHTML = "Skilled programmer";
-    document.getElementById('specialist_p').innerHTML = "I have years of experience in development of enterprise software";
- 
-    document.getElementById('graphic').innerHTML = "3D artist";
-    document.getElementById('graphic_p').innerHTML = "I also do some 3D Modelling & game development";
-
+    document.getElementById('specialist').innerHTML = "About me";
+    document.getElementById('specialist_p').innerHTML = "I have 5 years of professional experience with C# / .NET.<br><br>I  built some complex infrastructures consisting of server-side apis (ASP) and client apps (WPF, Xamarin). Terms like OOP, Design Patterns, Clean Code Principles are well known to me, as well as the current trends in the field and latest technology.<br><br> For the last 2 years I have also been increasedly interested in game development and 3D graphics. I use Blender as modelling software and chose the Unreal Engine over Unity as a game engine, as I wanted to challenge myself with something different than Unity´s C#. I also fell in love with the visual quality that UE offers straight out of the box ";
     
     document.getElementById('inboxme').innerHTML = "Inbox me:";
     document.getElementById('wien').innerHTML = "Vienna, Austria";
