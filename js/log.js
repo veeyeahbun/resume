47e6382e8a8ef2b3d9039ba3a3dc6ff97bbfef9f
Jan Ručil <rucil.jan@gmail.com>
2024-08-25 15:08:46 +0200

    .

--- js/log.js
+++ js/log.js
@@ -2,22 +2,32 @@
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
-    
-  var ip = '';
-
   var url = this.documentURI;
    // Fetch the IP address from the API
    fetch("https://ipinfo.io/json") 
    .then(response => response.json())
    .then(data => {
-       // Display the IP address on the screen
-       ip = `${data.ip}`;
-      
        return fetch('https://janrucil.dev/zigo/api/weblogs', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
-          body: '{"url":"'+ url +'","text":"served to '+ ip +'"}'         
+          body: '{"url":"'+ url +'","text":"content served to '+ `${data.ip}` +' in ' + `${data.city}` +'"}'         
       });
    }); 
+});
+
+
+document.getElementById("formsubmit").addEventListener("click", function(event){
+
+   var url = document.documentURI;
+    // Fetch the IP address from the API
+    fetch("https://ipinfo.io/json") 
+    .then(response => response.json())
+    .then(data => {   
+        return fetch('https://janrucil.dev/zigo/api/weblogs', {
+           method: 'POST',
+           headers: { 'Content-Type': 'application/json' },
+           body: '{"url":"'+ url +'","text":"form submitted by '+ `${data.ip}` +' in ' + `${data.city}` +'"}'               
+       });
+    }); 
 });
 
